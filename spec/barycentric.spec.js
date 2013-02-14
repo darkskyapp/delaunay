fd1007cd1ebeb0ac9815c6ee657335b79c842fcc
Jason LaPorte <ironwallaby@gmail.com>
2013-02-14 14:02:26 -0500

    Tests for the barycentric codes.

--- spec/barycentric.spec.js
+++ spec/barycentric.spec.js
@@ -4,10 +4,18 @@ var Barycentric = require("../lib/barycentric");
   "use strict";
 
   describe("Barycentric", function() {
-    var s2 = [
-          [ 0  , -Math.sqrt(3) / 3],
-          [-0.5,  Math.sqrt(3) / 6],
-          [ 0.5,  Math.sqrt(3) / 6]
+    var a  = Math.sqrt(3) / 6,
+        b  = 2 / Math.sqrt(6),
+        s2 = [
+          [ 0  , a * -2],
+          [-0.5, a     ],
+          [ 0.5, a     ]
+        ],
+        s3 = [
+          [ b,  b,  b],
+          [ b, -b, -b],
+          [-b,  b, -b],
+          [-b, -b,  b]
         ];
 
     describe("coordinates", function() {
@@ -47,7 +55,43 @@ var Barycentric = require("../lib/barycentric");
         expect(bary[1]).toBeCloseTo(1.488, 3);
       });
 
-      /* TODO: 3D cases */
+      it("should return 0,0,0 for the first coord of the reference tetrahedron", function() {
+        expect(Barycentric.coordinates(s3, s3[0])).toEqual([0, 0, 0]);
+      });
+
+      it("should return 1,0,0 for the second coord of the reference tetrahedron", function() {
+        expect(Barycentric.coordinates(s3, s3[1])).toEqual([1, 0, 0]);
+      });
+
+      it("should return 0,1,0 for the third coord of the reference tetrahedron", function() {
+        expect(Barycentric.coordinates(s3, s3[2])).toEqual([0, 1, 0]);
+      });
+
+      it("should return 0,0,1 for the fourth coord of the reference tetrahedron", function() {
+        expect(Barycentric.coordinates(s3, s3[3])).toEqual([0, 0, 1]);
+      });
+
+      it("should return 1/4,1/4,1/4 for 0,0,0 in the reference tetrahedron", function() {
+        expect(Barycentric.coordinates(s3, [0, 0, 0])).toEqual([1/4, 1/4, 1/4]);
+      });
+
+      it("should return 0.556,0.556,-0.056 for 0,0,-1 in the reference tetrahedron", function() {
+        var bary = Barycentric.coordinates(s3, [0, 0, -1]);
+        expect(Array.isArray(bary)).toBeTruthy();
+        expect(bary.length).toBe(3);
+        expect(bary[0]).toBeCloseTo( 0.556, 3);
+        expect(bary[1]).toBeCloseTo( 0.556, 3);
+        expect(bary[2]).toBeCloseTo(-0.056, 3);
+      });
+
+      it("should return -0.056,0.556,0.556 for 0,0,1 in the reference tetrahedron", function() {
+        var bary = Barycentric.coordinates(s3, [0, 0, 1]);
+        expect(Array.isArray(bary)).toBeTruthy();
+        expect(bary.length).toBe(3);
+        expect(bary[0]).toBeCloseTo(-0.056, 3);
+        expect(bary[1]).toBeCloseTo(-0.056, 3);
+        expect(bary[2]).toBeCloseTo( 0.556, 3);
+      });
     });
 
     describe("contains", function() {
@@ -79,8 +123,33 @@ var Barycentric = require("../lib/barycentric");
         expect(Barycentric.contains(s2, [0, 2])).toBeNull();
       });
 
-      /* TODO: 3D cases */
-      /* TODO: 3D cases */
+      it("should return 0,0,0 for the first coord of the reference tetrahedron", function() {
+        expect(Barycentric.contains(s3, s3[0])).toEqual([0, 0, 0]);
+      });
+
+      it("should return 1,0,0 for the second coord of the reference tetrahedron", function() {
+        expect(Barycentric.contains(s3, s3[1])).toEqual([1, 0, 0]);
+      });
+
+      it("should return 0,1,0 for the third coord of the reference tetrahedron", function() {
+        expect(Barycentric.contains(s3, s3[2])).toEqual([0, 1, 0]);
+      });
+
+      it("should return 0,0,1 for the fourth coord of the reference tetrahedron", function() {
+        expect(Barycentric.contains(s3, s3[3])).toEqual([0, 0, 1]);
+      });
+
+      it("should return 1/4,1/4,1/4 for 0,0,0 in the reference tetrahedron", function() {
+        expect(Barycentric.contains(s3, [0, 0, 0])).toEqual([1/4, 1/4, 1/4]);
+      });
+
+      it("should return null for 0,0,-1 in the reference tetrahedron", function() {
+        expect(Barycentric.contains(s3, [0, 0, -1])).toBeNull();
+      });
+
+      it("should return null for 0,0,1 in the reference tetrahedron", function() {
+        expect(Barycentric.contains(s3, [0, 0, 1])).toBeNull();
+      });
     });
   });
 }());
